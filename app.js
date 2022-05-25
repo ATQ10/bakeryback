5fb1926c8ebc49df96daa49b1f8a10641bf98aa1
Amaury Romo <amauryromohdz9@gmail.com>
2022-05-24 23:51:08 -0500

    Add some other dependencies and project architechture

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express"),
+  app = express(),
+  mongoose = require('mongoose');
+
+require("dotenv")
+  .config();
+
+//Connect to database
+try {
+    mongoose.connect("mongodb://localhost:27017/usersdb", {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log("connected to db");
+  } catch (error) {
+    handleError(error);
+  }
+  process.on('unhandledRejection', error => {
+    console.log('unhandledRejection', error.message);
+  });
+
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({
+  extended: true
+}));
+
+//setup server to listen on port 3030
+app.listen(process.env.PORT || 3030, () => {
+  console.log("Server is live on port 3030");
+})
---
+++ models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    userImage:{
+        type: String,
+        required: [true,"An user image is required!"]
+    },
+    fullName: {
+        type: String,
+        required: [true, "fullname not provided!"],
+      },
+      email: {
+        type: String,
+        unique: [true, "email already exists in database!"],
+        lowercase: true,
+        trim: true,
+        required: [true, "email not provided"],
+        validate: {
+          validator: function (v) {
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+          },
+          message: '{VALUE} is not a valid email!'
+        }
+    
+      },
+      role: {
+        type: String,
+        enum: ["normal", "admin"],
+        required: [true, "Please specify user role"]
+      },
+      password: {
+        type: String,
+        required: true
+      },
+      created: {
+        type: Date,
+        default: Date.now
+      },
+      position:{
+        type: String,
+        enum:["sales","secretary"]
+      },
+      plant:{
+        type: Schema.Types.ObjectId, 
+        ref: 'Plant' 
+      }
+});
+
+module.exports = mongoose.model('User', userSchema);
